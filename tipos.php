de8496ec11e032b15a3bd9d7cb3b1cdbbdd71db1
Jefferson Soares <soares.jeffersonthadeu@gmail.com>
2023-03-12 13:55:05 -0300

    commit

---
+++ tipos.php
@@ -0,0 +1,38 @@
+<?php
+
+
+$idade = 21;
+$salario = 1000.30;
+
+$divisao = 10 / 3;
+
+$texto = "Olá Mundo!";
+
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($idade);
+
+echo gettype($salario);
+
+echo $divisao;
+
+echo $texto;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
