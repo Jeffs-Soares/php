9cb6992a1f5731385193a2a2827098b321a7bc7a
Jefferson Soares <soares.jeffersonthadeu@gmail.com>
2023-03-12 13:44:54 -0300

    commit

---
+++ operacoes.php
@@ -0,0 +1,28 @@
+<?php
+
+$idade = 21;
+
+$idadeDaqui10Anos = $idade + 10;
+
+echo $idadeDaqui10Anos;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicaçao = 2 * 2;
+$divisao = 2 / 2;
+
+$resto = 3 % 10;
+$potenciacao = 2 ** 3;
+
+
+
+
+
+
+
+
+
+
+
+
+
