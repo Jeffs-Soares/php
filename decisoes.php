1abd3641f654143a1adc024d4b3172bb2dc0b1cd
Jefferson Soares <soares.jeffersonthadeu@gmail.com>
2023-03-13 18:52:04 -0300

    commit

--- decisoes.php
+++ decisoes.php
@@ -1,19 +1,18 @@
-
 <?php
 
-
 $idade = 18;
+$numeroDePessoas = 2;
 
-echo "Você só pode entrar se tiver mais de 18 anos." . PHP_EOL;
+echo "Você só pode entrar se tiver a partir de 18 anos ou ";
+echo "a partir de 16 anos acompanhado" . PHP_EOL;
 
-if ($idade >= 18 /* || $idade == 18 */)
-{
-
-    echo "Você tem $idade anos. Pode entrar.";
-}else{
-    echo "Você não tem mais de 18 anos parça.";
+if ($idade >= 18) {
+    echo "Você tem $idade anos. Pode entrar sozinho.";
+} elseif ($idade >= 16 && $numeroDePessoas > 1) {
+    echo "Você tem $idade anos, está acompanhado(a), então pode entrar.";
+} else {
+    echo "Você só tem $idade anos. Você não pode entrar.";
 }
 
-
-
-
+echo PHP_EOL;
+echo "Adeus!";
