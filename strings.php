71cee08c356feacae656b273ba226dc3ec2e86cc
Jefferson Soares <soares.jeffersonthadeu@gmail.com>
2023-03-13 06:47:05 -0300

    commit

---
+++ strings.php
@@ -0,0 +1,34 @@
+<?php
+
+$idade = 21;
+$nome = "Jeff";
+
+echo 'Olá ' . $idade;
+
+echo PHP_EOL; // Quebra de linha da melhor forma
+
+/* echo "\n";
+
+echo "\t"; */
+
+echo "Essa é minha $idade e meu nome é $nome";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
