e37c0142f91a5357c5d33efd018467d0c16af716
Jefferson Soares <soares.jeffersonthadeu@gmail.com>
2023-05-05 12:15:59 -0300

    strings

--- Manipulando textos com PHP - Strings/inicioFim.php
+++ Manipulando textos com PHP - Strings/inicioFim.php
@@ -1,6 +1,12 @@
-
-
 <?php
 
-
 $url = 'https://alura.com.br';
+
+if(str_starts_with($url, 'https')){
+    echo 'É segura';
+}else{
+    echo 'Não segura';
+};
+
+//str_starts_with()
+//str_ends_with()
