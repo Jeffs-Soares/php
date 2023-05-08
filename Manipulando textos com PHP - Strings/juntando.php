344b930bf7225c737cbd3653bfd3139f3f40a034
Jefferson Soares <soares.jeffersonthadeu@gmail.com>
2023-05-08 09:57:38 -0300

    strings

---
+++ Manipulando textos com PHP - Strings/juntando.php
@@ -0,0 +1,9 @@
+<?php
+
+$telefones = ['(38) 98811 - 6632' , '(21) 99999 - 9999', '(21) 2222 - 2222'];
+
+
+echo implode(PHP_EOL, $telefones) . PHP_EOL;
+
+//União de arrays
+
---
+++ Manipulando textos com PHP - Strings/removerEspaços.php
@@ -0,0 +1,8 @@
+<?php
+
+$email = ' jeff@test.com ';
+
+echo trim($email) . PHP_EOL;
+echo ltrim($email) . PHP_EOL;
+echo rtrim($email) . PHP_EOL;
+echo $email;
