197864a4a81850b2d972813b402fbebdb4151b2b
Jefferson Soares <soares.jeffersonthadeu@gmail.com>
2023-05-09 09:54:59 -0300

    POO

--- 05 - Orientação a Objetos com PHP - Introdução/projeto/src/Conta.php
+++ 05 - Orientação a Objetos com PHP - Introdução/projeto/src/Conta.php
@@ -6,6 +6,18 @@ class Conta{
     public string $nomeTitular;
     public float $saldo;
 
+    public function sacar(float $valorASacar){
+
+        if($valorASacar > $this -> saldo){
+            echo 'Saldo indisponível!';
+        }else{
+
+            $this -> saldo -= $valorASacar;
+
+        }
+
+    }
+
     
 }
 
@@ -14,7 +26,10 @@ $primeiraConta = new Conta();
 
 $primeiraConta -> saldo = 200;
 
-echo $primeiraConta -> saldo;
+echo $primeiraConta -> saldo . PHP_EOL;
+
+$primeiraConta->sacar(20);
+$primeiraConta->sacar(500);
 
 
-//var_dump($primeiraConta);
+var_dump($primeiraConta);
