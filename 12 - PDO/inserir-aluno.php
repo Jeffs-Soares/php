c3656726c315981b9669c461d990b5f6ea3fffeb
Jefferson Soares <soares.jeffersonthadeu@gmail.com>
2023-05-22 14:46:15 -0300

    PHP PDO

--- 12 - PDO/inserir-aluno.php
+++ 12 - PDO/inserir-aluno.php
@@ -1,11 +1,11 @@
 <?php
 
 use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
 
 require_once 'vendor/autoload.php';
 
-$databasePath = __DIR__ . '/banco.sqlite';
-$pdo = new PDO('sqlite:' . $databasePath);
+$pdo = ConnectionCreator::createConnection();
 
 $student = new Student(
     null,
@@ -16,7 +16,7 @@ $name = $student->name();
 
 $sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindParam(':name', $student->name());
+$statement->bindValue(':name', $student->name());
 $statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 
 if ($statement->execute()) {
