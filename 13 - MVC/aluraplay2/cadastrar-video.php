a9c1e42a16f6e40d98d54a568d38b785706ad198
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2023-06-02 16:40:04 -0300

    PHP MVC

---
+++ 13 - MVC/aluraplay2/cadastrar-video.php
@@ -0,0 +1,41 @@
+<?php 
+
+
+require_once "./conn.php";
+
+
+$sql = "INSERT INTO videos (url , title) VALUES (:url, :title);";
+
+$statment = $conn -> prepare($sql);
+
+
+
+$url = filter_input(INPUT_POST,  $_POST['url'] , FILTER_VALIDATE_URL);
+
+
+if($url === false){
+    header("Location: /index.php?sucesso=0");
+}
+
+
+$titulo = filter_input(INPUT_POST, $_POST['titulo']);
+
+
+$statment -> bindValue(":url", $url);
+$statment -> bindValue(":title", $titulo);
+
+
+
+
+if($statment -> execute() === false ){
+
+    header("Location:/index.php?sucesso=0");
+}else{
+    
+    header("Location:/index.php?sucesso=1");
+}
+
+
+
+
+
