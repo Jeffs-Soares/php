38c4781f3e3ebd0cad240d934cbc1396aa65cc72
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2023-06-02 12:07:53 -0300

    PHP MVC

---
+++ 13 - MVC/aluraplay2/conn.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+$host = "localhost";
+$port = "5432";
+$dbname = "alura";
+
+$user = "postgres";
+$password = "root";
+
+
+
+try{
+
+    $conn = new \PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+}catch(PDOException $e){
+
+    die($e -> getMessage());
+
+}
+
+
+
+
+
+
+
