c577a732a8dce0a75ee8cf50fd2c758e8effaa3c
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2023-06-13 10:53:28 -0300

    PHP MVC

--- 13 - MVC/aluraplay2/public/index.php
+++ 13 - MVC/aluraplay2/public/index.php
@@ -1,27 +1,32 @@
 <?php
 use Alura\Mvc\Controller\VideoListController;
-
+use Alura\Mvc\Repository\VideoRepository;
 
 //declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$host = "localhost";
+$port = "5432";
+$dbname = "alura";
+$user = "postgres";
+$password = "root";
 
+try {
+     $pdo = new \PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
+} catch (\PDOException $e) {
+     die($e->getMessage());
+}
 
-
-
-
-
-
-
+$videoRepository = new VideoRepository($pdo);
 
 
 
 if (!array_key_exists('PATH_INFO', $_SERVER) || $_SERVER['PATH_INFO'] === '/') {
 
-     $controller = new VideoListController();
+     $controller = new VideoListController($videoRepository);
      $controller ->processaRequisicao();
-     
+
 } elseif ($_SERVER['PATH_INFO'] === '/cadastrar-video') {
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
          require_once __DIR__ . '/../formulario.php';
--- 13 - MVC/aluraplay2/src/Controller/VideoListController.php
+++ 13 - MVC/aluraplay2/src/Controller/VideoListController.php
@@ -6,23 +6,10 @@ use Alura\Mvc\Repository\VideoRepository;
 
 class VideoListController
 {
-    private VideoRepository $videoRepository;
 
-    public function __construct()
+    public function __construct( private VideoRepository $videoRepository)
     {
-        $host = "localhost";
-        $port = "5432";
-        $dbname = "alura";
-        $user = "postgres";
-        $password = "root";
-
-        try {
-            $pdo = new \PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
-        } catch (\PDOException $e) {
-            die($e->getMessage());
-        }
-
-        $this->videoRepository = new VideoRepository($pdo);
+       
 
     }
 
