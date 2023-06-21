d5e8fbf9fa5bcf2cf37fb30865b2092039b2fe1b
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2023-06-21 10:26:01 -0300

    PHP Segurança

---
+++ 14 - PHP na Web: lidando com segurança e API/aluraplay/public/index.php
@@ -0,0 +1,45 @@
+<?php
+
+use Alura\Mvc\Controller\Error404Controller;
+use Alura\Mvc\Repository\VideoRepository;
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+//BD CONNECTION
+
+$host = "localhost";
+$port = "5432";
+$dbname = "alura";
+$user = "postgres";
+$password = "root";
+
+try {
+     $pdo = new \PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
+} catch (\PDOException $e) {
+     die($e->getMessage());
+}
+
+// End BD CONNECTION 
+
+$videoRepository = new VideoRepository($pdo);
+
+$routes = require_once __DIR__ . '/../config/routes.php';
+
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+$httpMethod =  $_SERVER['REQUEST_METHOD'];
+
+$key = "$httpMethod|$pathInfo";
+
+if(array_key_exists($key, $routes)){
+
+     $controllerClass = $routes[$key];
+     $controller = new $controllerClass($videoRepository);
+     
+}else{
+     $controller = new Error404Controller();
+};
+
+
+
+$controller -> processaRequisicao();
