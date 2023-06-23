9a99f6af499523304617c63c9064b20813622f65
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2023-06-23 11:56:12 -0300

    PHP Segurança

--- 14 - PHP: Segurança e API/aluraplay/src/Controller/EditVideoController.php
+++ 14 - PHP: Segurança e API/aluraplay/src/Controller/EditVideoController.php
@@ -34,8 +34,19 @@ class EditVideoController implements Controller
             return;
         }
 
-        $video = new Video($url, $titulo);
+        $video = new Video($url, $titulo);  
         $video->setId($id);
+
+        if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
+            move_uploaded_file(
+                $_FILES['image']['tmp_name'],
+                __DIR__ . '/../../public/img/uploads/' . $_FILES['image']['name']
+
+            );
+
+            $video->setFilePath($_FILES['image']['name']);
+
+        }
 
         $success = $this->videoRepository->update($video);
 
