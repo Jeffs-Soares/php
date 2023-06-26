2820eb0ac665277a0f00fb0e198c4ee20b2f1eec
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2023-06-26 15:14:00 -0300

    PHP Segurança

--- 14 - PHP: Segurança e API/aluraplay/config/routes.php
+++ 14 - PHP: Segurança e API/aluraplay/config/routes.php
@@ -2,12 +2,14 @@
 
 use Alura\Mvc\Controller\DeleteVideoController;
 use Alura\Mvc\Controller\EditVideoController;
+use Alura\Mvc\Controller\JsonVideoListController;
 use Alura\Mvc\Controller\NewVideoController;
 use Alura\Mvc\Controller\VideoFormController;
 use Alura\Mvc\Controller\VideoListController;
 use Alura\Mvc\Controller\LoginFormController;
 use Alura\Mvc\Controller\LoginController;
 use Alura\Mvc\Controller\LogoutController;
+use Alura\Mvc\Controller\NewJsonVideoController;
 
 return [
     'GET|/'                 => VideoListController::class,
@@ -19,6 +21,8 @@ return [
     'GET|/login'            => LoginFormController::class,
     'POST|/login'           => LoginController::class,
     'GET|/logout'           => LogoutController::class,
+    'GET|/videos-json'       => JsonVideoListController::class, //API ROUTE
+    'POST|/videos'       => NewJsonVideoController::class,      //API ROUTE
 
 ];
 
