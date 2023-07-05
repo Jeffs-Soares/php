cf7a5613ac034138b7f23485148222c53380b624
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2023-07-05 14:37:13 -0300

    PHP

--- 06 - POO: Herança, Polimorfismo e Interfaces/projeto/banco.php
+++ 06 - POO: Herança, Polimorfismo e Interfaces/projeto/banco.php
@@ -7,9 +7,10 @@ use Alura\Banco\Modelo\Endereco;
 use Alura\Banco\Modelo\CPF;
 use Alura\Banco\Modelo\Conta\Conta;
 
+
 $endereco = new Endereco('Petrópolis', 'um bairro', 'minha rua', '71B');
 $vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $endereco);
-$primeiraConta = new Conta($vinicius);
+//$primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
 
@@ -18,10 +19,10 @@ echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
 $patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $endereco);
-$segundaConta = new Conta($patricia);
+//$segundaConta = new Conta($patricia);
 var_dump($segundaConta);
 
 $outroEndereco = new Endereco('A', 'b', 'c', '1D');
-$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
+//$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
