97179831697686569f4be1226fecb81f2cfbd31a
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2023-07-10 11:41:57 -0300

    refactor: pdo extraction for another file

--- 14 - PHP: Segurança e API/aluraplay/public/index.php
+++ 14 - PHP: Segurança e API/aluraplay/public/index.php
@@ -5,21 +5,7 @@ use Alura\Mvc\Repository\VideoRepository;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-//BD CONNECTION
-
-$host = "localhost";
-$port = "5432";
-$dbname = "alura";
-$user = "postgres";
-$password = "root";
-
-try {
-     $pdo = new \PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
-} catch (\PDOException $e) {
-     die($e->getMessage());
-}
-
-// End BD CONNECTION 
+$pdo = require_once __DIR__ . '/../config/conn.php';
 
 $videoRepository = new VideoRepository($pdo);
 
